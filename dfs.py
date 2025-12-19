8c148c5913abb0e5a9fc4940cec5672448893484
Abdo Atia <atia61035@gmail.com>
2025-12-19 13:50:10 +0200

    Refactor DFS implementation to use iterative approach

--- dfs.py
+++ dfs.py
@@ -1,31 +1,16 @@
+def dfs(grid, start, goal, valid, moves):
+    stack = [(start, [start])]
+    visited = {start}
 
-def dfs(grid, start, goal, moves):
-    rows = len(grid)
-    cols = len(grid[0])
-    visited = [[False]*cols for _ in range(rows)]
-    path = []
+    while stack:
+        node, path = stack.pop()
 
-    def dfs_util(x, y):
-        if x < 0 or y < 0 or x >= rows or y >= cols:
-            return False
-
-        if grid[x][y] == 1 or visited[x][y]:
-            return False
-
-        visited[x][y] = True
-        path.append((x, y))
-
-        if (x, y) == goal:
-            return True
+        if node == goal:
+            return path
 
         for dx, dy in moves:
-            if dfs_util(x + dx, y + dy):
-                return True
+            nx, ny = node[0] + dx, node[1] + dy
+            if valid(nx, ny) and (nx, ny) not in visited:
+                visited.add((nx, ny))
+                stack.append(((nx, ny), path + [(nx, ny)]))
 
-        path.pop()
-        return False
-
-    if dfs_util(start[0], start[1]):
-        return path
-    else:
-        return None
